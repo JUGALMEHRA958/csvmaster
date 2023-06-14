6a14ed341f02b129a3d8b5f75f3df58738e07bda
Jugal Mehra <jugal.mehra@indianic.com>
2023-06-14 18:45:50 +0530

    chaneg

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const path = require('path');
 const app = express();
 require('dotenv').config(); // Import the habits router
 const ejs = require('ejs')
-
+// const express = require('express');
+// const app = express();
+const multer = require('multer');
+const csvParser = require('csv-parser');
+const fs = require('fs');
 
 //setting template engine
 app.set('view engine', 'ejs');
@@ -13,6 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.json());
+const upload = multer({ dest: 'uploads/' });
 
 //start app function 
 const startApp = ()=>{
@@ -31,6 +36,26 @@ mongoose.connect(dbUrl).then(startApp);
 
 //route for home 
 app.get("/", (req,res,next)=>{
-    res.render('home' , {files:[]})
+    res.render('home' , {files:[],data:null})
 })
+
+// Handle file upload and provide CSV data as JSON response
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+    const filePath = req.file.path;
+    const results = [];
+  
+    fs.createReadStream(filePath)
+      .pipe(csvParser())
+      .on("data", (data) => {
+        results.push(Object.values(data));
+      })
+      .on("end", () => {
+        fs.unlinkSync(filePath); // Remove the uploaded file
+        const files = fs.readdirSync("uploads/");
+        const data = results;
+        // Return the CSV data as JSON response
+        res.render("home", { files: files, data: data }); // Replace `csvData` with the actual data from the CSV file
+      });
+
+  });
 
